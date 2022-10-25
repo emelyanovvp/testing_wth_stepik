bebb4d67723c7134e815dcd5d8674303cf3ae159
emelyanovvp <emelyanovvp@mail.ru>
2022-10-25 15:38:43 +0300

    renamed unittest

--- main.py
+++ main.py
@@ -1,39 +1,41 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-import time
 import unittest
 link1 = "http://suninjuly.github.io/registration1.html"
 link2 = "http://suninjuly.github.io/registration2.html"
 class TestReg(unittest.TestCase):
     def test_reg1(self):
         browser = webdriver.Chrome()
+        browser.implicitly_wait(1)
         browser.get(link1)
-        input1 = browser.find_element(By.XPATH, "/html/body/div[1]/form/div[1]/div[1]/input")
-        input1.send_keys("Ivan")
-        input2 = browser.find_element(By.XPATH, "/html/body/div[1]/form/div[1]/div[2]/input")
-        input2.send_keys("Petrov")
-        input3 = browser.find_element(By.XPATH, "/html/body/div[1]/form/div[1]/div[3]/input")
-        input3.send_keys("Smolensk@mail.ru")
-        button = browser.find_element(By.XPATH, "/html/body/div[1]/form/button").click()
-        time.sleep(1)
-        welcome_text_elem = browser.find_element(By.XPATH, "/html/body")
+        first_name = browser.find_element(By.CSS_SELECTOR, "div.first_block input.form-control.first")
+        first_name.send_keys("Ivan")
+        last_name = browser.find_element(By.CSS_SELECTOR, "div.first_block input.form-control.second")
+        last_name.send_keys("Smirnov")
+        email = browser.find_element(By.CSS_SELECTOR, "div.first_block input.form-control.third")
+        email.send_keys("Ivan@mail.ru")
+        button = browser.find_element(By.CSS_SELECTOR, "button.btn")
+        button.click()
+        welcome_text_elt = browser.find_element(By.TAG_NAME, "h1")
         welcome_text = welcome_text_elem.text
         self.assertEqual(welcome_text, "Congratulations! You have successfully registered!")
         browser.quit()
     def test_reg2(self):
         browser = webdriver.Chrome()
+        browser.implicitly_wait(1)
         browser.get(link2)
-        input1 = browser.find_element(By.XPATH, "/html/body/div[1]/form/div[1]/div[1]/input")
-        input1.send_keys("Ivan")
-        input2 = browser.find_element(By.XPATH, "/html/body/div[1]/form/div[1]/div[2]/input")
-        input2.send_keys("Petrov")
-        input3 = browser.find_element(By.XPATH, "/html/body/div[1]/form/div[1]/div[3]/input")
-        input3.send_keys("Smolensk@mail.ru")
-        button = browser.find_element(By.XPATH, "/html/body/div[1]/form/button").click()
-        time.sleep(1)
-        welcome_text_elem = browser.find_element(By.XPATH, "/html/body")
+        first_name = browser.find_element(By.CSS_SELECTOR, "div.first_block input.form-control.first")
+        first_name.send_keys("Ivan")
+        last_name = browser.find_element(By.CSS_SELECTOR, "div.first_block input.form-control.second")
+        last_name.send_keys("Smirnov")
+        email = browser.find_element(By.CSS_SELECTOR, "div.first_block input.form-control.third")
+        email.send_keys("Ivan@mail.ru")
+        button = browser.find_element(By.CSS_SELECTOR, "button.btn")
+        button.click()
+
+        welcome_text_elt = browser.find_element(By.TAG_NAME, "h1")
         welcome_text = welcome_text_elem.text
         self.assertEqual(welcome_text, "Congratulations! You have successfully registered!")
         browser.quit()
 if __name__ == '__main__':
-    unittest.main(warnings='ignore') # for PyTest input "pytest main.py" in Terminal 
+    unittest.main(warnings='ignore')
